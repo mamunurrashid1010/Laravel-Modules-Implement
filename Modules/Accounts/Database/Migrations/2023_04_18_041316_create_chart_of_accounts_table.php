c8f103db16a2a3fd2b16fdf06444ce38a966ebff
mamunurrashid1010 <mamunurrashid1010@gmail.com>
2023-04-18 10:15:49 +0600

    Create ChartOfAccount controller, model, migration file in the Accounts module

---
+++ Modules/Accounts/Database/Migrations/2023_04_18_041316_create_chart_of_accounts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChartOfAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chart_of_accounts', function (Blueprint $table) {
+            $table->id();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chart_of_accounts');
+    }
+}
---
+++ Modules/Accounts/Entities/ChartOfAccount.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Accounts\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ChartOfAccount extends Model
+{
+    protected $fillable = [];
+}
